4fc63eca623114838c919dcd8d8a14fadd1d57c1
Pronoy-dev <propronoy3@gmail.com>
2024-11-30 17:39:09 +0600

    node 3 done

--- data.js
+++ data.js
@@ -1,4 +1,24 @@
-let data =
-  " iokfjwie9hfi9wehfi9hwifhnuifhuifuihuifbhjbsduifjwlfvhiuwsehgfvoiwsevckj;bsdoicnsdjkvb fodv loiksgbvoiavc lkwdesnbcpiuwehoicfbsd[pifvchso'viuwsvfhoklsdbvuisdjvskdbvjsdhvjksdbvcfisdbv sdhb]";
+const studentList = [
+  {
+    id: 1,
+    name: "Pronoy",
+    batch: "mern2307",
+  },
+  {
+    id: 2,
+    name: "naim",
+    batch: "mern2307",
+  },
+  {
+    id: 3,
+    name: "redwan",
+    batch: "mern2307",
+  },
+  {
+    id: 4,
+    name: "nahid",
+    batch: "mern2307",
+  },
+];
 
-module.exports = { data };
+module.exports = { studentList };
--- main.js
+++ main.js
@@ -1,13 +1,59 @@
-const { log } = require("console");
-const path = require("path");
+const http = require("http");
 const fs = require("fs");
-const urlPath = "C:\\temp\\myfile.html";
+const url = require("url");
+const { studentList } = require("./data");
+const mainPath = require("path");
+const { count } = require("console");
 
-const pathName = path.join(__dirname, "image/temp.txt");
-const rightPath = path.normalize(pathName);
+const server = http.createServer(function (req, res) {
+  const routes = url.parse(req.url);
+  const { path, pathname } = routes;
+  const { query } = url.parse(path);
+  const queryId = parseInt(query?.split("=")[1]);
+  let countFilePath = mainPath.join(__dirname, "count.txt");
+  let statusFilePath = mainPath.join(__dirname, "status.txt");
+  fs.readFile(countFilePath, "utf8", (err, data) => {
+    let count = 0;
+    if (!err && data) {
+      count = parseInt(data, 10);
+    }
 
-fs.writeFile(rightPath, "some text here", (err) => {
-  if (err) {
-    return;
+    count++;
+
+    fs.writeFile(countFilePath, count.toString(), (err) => {
+      if (err) {
+        return;
+      }
+      fs.appendFile(statusFilePath, `${count}) ${req.url} \n`, (err) => {
+        if (err) {
+          console.error("error");
+        }
+      });
+    });
+  });
+
+  if (path == "/data") {
+    const jsonStudentlist = JSON.stringify(studentList);
+    res.end(jsonStudentlist);
   }
+
+  if (path == "/product") {
+    fetch("https://dummyjson.com/products")
+      .then((res) => res.json())
+      .then((data) => {
+        res.end(JSON.stringify(data));
+      });
+  }
+
+  // get single product routes
+  if (`${pathname}/?id=${query}` == `${pathname}/?id=${query}`) {
+    fetch(`https://dummyjson.com/products/${queryId}`)
+      .then((res) => res.json())
+      .then((data) => {
+        res.end(JSON.stringify(data));
+      });
+  }
+});
+server.listen(4000, () => {
+  console.log("Server running on  4000 port");
 });
